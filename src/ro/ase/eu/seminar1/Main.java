c455f72792523ea5a19c1f3efe71d8552576b2ff
MihaiD <mihaid_b@protonmail.com>
2021-02-27 11:22:25 +0200

    Wild Cat

--- src/ro/ase/eu/seminar1/Main.java
+++ src/ro/ase/eu/seminar1/Main.java
@@ -1,6 +1,7 @@
 package ro.ase.eu.seminar1;
 import clase.Animal;
 import clase.Lion;
+import clase.WildCat;
 import clase.Zoo;
 public class Main {
 public static void main(String[] args) {
@@ -10,6 +11,13 @@ public static void main(String[] args) {
     Animal lion3 = new Lion("Leo");
     zoo.addAnimal(lion1);
     zoo.addAnimal(lion2);
+    zoo.feedAllAnimals();
+
+    WildCat cat = new WildCat("kitty");
+    WildCat cat1 = new WildCat("softkitty");
+    //MET PROC N-AU OPERATII LA CONSOLA
+    zoo.addAnimal(cat);
+    zoo.addAnimal(cat1);
     zoo.feedAllAnimals();
 }
 }
