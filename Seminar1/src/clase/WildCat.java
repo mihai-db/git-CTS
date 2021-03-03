a55c4aaff7ac5240ae8d107f92b0ffdc7e54448b
MihaiD <mihaid_b@protonmail.com>
2021-03-03 11:11:44 +0200

    hierarchy organising

---
+++ Seminar1/src/clase/WildCat.java
@@ -0,0 +1,23 @@
+package clase;
+
+public class WildCat extends Animal {
+public String food;
+
+    public WildCat(String nume) {
+
+        super(nume);
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public WildCat(String nume, String food) {
+        super(nume);
+        this.food = food;
+    }
+}
---
+++ Seminar1/src/ro/ase/eu/seminar1/Main.java
@@ -0,0 +1,24 @@
+package ro.ase.eu.seminar1;
+import clase.Animal;
+import clase.Lion;
+import clase.WildCat;
+import clase.Zoo;
+public class Main {
+public static void main(String[] args) {
+    Zoo zoo = new Zoo();
+    Lion lion1 = new Lion("leu");
+    Lion lion2 = new Lion("altLeu");
+    Animal lion3 = new Lion("RegeleLeu");
+    zoo.addAnimal(lion1);
+    zoo.addAnimal(lion2);
+    zoo.feedAllAnimals();
+
+    WildCat aSmallCat = new WildCat("kitty");
+    WildCat aFurballCat = new WildCat("softkitty");
+    //MET PROC N-AU OPERATII LA CONSOLA
+    zoo.addAnimal(aSmallCat);
+    zoo.addAnimal(aFurballCat);
+    zoo.feedAllAnimals();
+    //Interfata are doar fct virtuale, in Java are doar metode abstracte
+}
+}
